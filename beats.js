5c14130b223f7728afe9c43a75fd2fa67f45ac33
A-Kubista <aleksanderkubista@gmail.com>
2024-10-20 20:15:40 +0200

    first commit

---
+++ beats.js
@@ -0,0 +1,257 @@
+      const beatWithTimestamps = [
+  {
+    "timestamp": 977,
+    "drum": true,
+    "bass": false
+  },
+  {
+    "timestamp": 1708,
+    "drum": false,
+    "bass": true
+  },
+  {
+    "timestamp": 2533,
+    "drum": true,
+    "bass": false
+  },
+  {
+    "timestamp": 3334,
+    "drum": false,
+    "bass": true
+  },
+  {
+    "timestamp": 4151,
+    "drum": true,
+    "bass": false
+  },
+  {
+    "timestamp": 4926,
+    "drum": false,
+    "bass": true
+  },
+  {
+    "timestamp": 5821,
+    "drum": true,
+    "bass": false
+  },
+  {
+    "timestamp": 6619,
+    "drum": false,
+    "bass": true
+  },
+  {
+    "timestamp": 7472,
+    "drum": true,
+    "bass": false
+  },
+  {
+    "timestamp": 8285,
+    "drum": false,
+    "bass": true
+  },
+  {
+    "timestamp": 9149,
+    "drum": true,
+    "bass": false
+  },
+  {
+    "timestamp": 9949,
+    "drum": false,
+    "bass": true
+  },
+  {
+    "timestamp": 10787,
+    "drum": true,
+    "bass": false
+  },
+  {
+    "timestamp": 11624,
+    "drum": false,
+    "bass": true
+  },
+  {
+    "timestamp": 12483,
+    "drum": true,
+    "bass": false
+  },
+  {
+    "timestamp": 12873,
+    "drum": true,
+    "bass": false
+  },
+  {
+    "timestamp": 13276,
+    "drum": false,
+    "bass": true
+  },
+  {
+    "timestamp": 14147,
+    "drum": true,
+    "bass": false
+  },
+  {
+    "timestamp": 14973,
+    "drum": false,
+    "bass": true
+  },
+  {
+    "timestamp": 15827,
+    "drum": true,
+    "bass": false
+  },
+  {
+    "timestamp": 16657,
+    "drum": false,
+    "bass": true
+  },
+  {
+    "timestamp": 17459,
+    "drum": true,
+    "bass": false
+  },
+  {
+    "timestamp": 18303,
+    "drum": false,
+    "bass": true
+  },
+  {
+    "timestamp": 19159,
+    "drum": true,
+    "bass": false
+  },
+  {
+    "timestamp": 19874,
+    "drum": false,
+    "bass": true
+  },
+  {
+    "timestamp": 20798,
+    "drum": true,
+    "bass": false
+  },
+  {
+    "timestamp": 21623,
+    "drum": false,
+    "bass": true
+  },
+  {
+    "timestamp": 22502,
+    "drum": true,
+    "bass": false
+  },
+  {
+    "timestamp": 23304,
+    "drum": false,
+    "bass": true
+  },
+  {
+    "timestamp": 24163,
+    "drum": true,
+    "bass": false
+  },
+  {
+    "timestamp": 25028,
+    "drum": false,
+    "bass": true
+  },
+  {
+    "timestamp": 25793,
+    "drum": true,
+    "bass": false
+  },
+  {
+    "timestamp": 26234,
+    "drum": true,
+    "bass": false
+  },
+  {
+    "timestamp": 26695,
+    "drum": false,
+    "bass": true
+  },
+  {
+    "timestamp": 27628,
+    "drum": true,
+    "bass": false
+  },
+  {
+    "timestamp": 28266,
+    "drum": false,
+    "bass": true
+  },
+  {
+    "timestamp": 29123,
+    "drum": true,
+    "bass": false
+  },
+  {
+    "timestamp": 29919,
+    "drum": false,
+    "bass": true
+  },
+  {
+    "timestamp": 30785,
+    "drum": true,
+    "bass": false
+  },
+  {
+    "timestamp": 31617,
+    "drum": false,
+    "bass": true
+  },
+  {
+    "timestamp": 32481,
+    "drum": true,
+    "bass": false
+  },
+  {
+    "timestamp": 33296,
+    "drum": false,
+    "bass": true
+  },
+  {
+    "timestamp": 34116,
+    "drum": true,
+    "bass": false
+  },
+  {
+    "timestamp": 34966,
+    "drum": false,
+    "bass": true
+  },
+  {
+    "timestamp": 35819,
+    "drum": true,
+    "bass": false
+  },
+  {
+    "timestamp": 36613,
+    "drum": false,
+    "bass": true
+  },
+  {
+    "timestamp": 37440,
+    "drum": true,
+    "bass": false
+  },
+  {
+    "timestamp": 38286,
+    "drum": false,
+    "bass": true
+  },
+  {
+    "timestamp": 39152,
+    "drum": true,
+    "bass": false
+  },
+  {
+    "timestamp": 39557,
+    "drum": true,
+    "bass": false
+  },
+  {
+    "timestamp": 39982,
+    "drum": false,
+    "bass": true
+  }
+];
---
+++ lyrics.js
@@ -0,0 +1,562 @@
+   const lyricsWithTimestamps = [
+            {
+                "text": "Hej Wy",
+                "timestamp": 14653
+            },
+            {
+                "text": "Gdzie byliscie",
+                "timestamp": 15785
+            },
+            {
+                "text": "wczesniej",
+                "timestamp": 16616
+            },
+            {
+                "text": "Kiedy",
+                "timestamp": 17875
+            },
+            {
+                "text": "czulam bol i strach",
+                "timestamp": 19133
+            },
+            {
+                "text": "Dawniej",
+                "timestamp": 21294
+            },
+            {
+                "text": "Przyjaciele byli",
+                "timestamp": 22161
+            },
+            {
+                "text": "wszystkim",
+                "timestamp": 23224
+            },
+            {
+                "text": "Dzisiaj",
+                "timestamp": 24646
+            },
+            {
+                "text": "Dzis samemu",
+                "timestamp": 25509
+            },
+            {
+                "text": "trzeba grac",
+                "timestamp": 26399
+            },
+            {
+                "text": "Wolaj",
+                "timestamp": 27939
+            },
+            {
+                "text": "Krzycze glosno",
+                "timestamp": 29065
+            },
+            {
+                "text": "przez sen",
+                "timestamp": 29957
+            },
+            {
+                "text": "Cisza",
+                "timestamp": 31301
+            },
+            {
+                "text": "Tylko cisze",
+                "timestamp": 32158
+            },
+            {
+                "text": "umiesz dac",
+                "timestamp": 32938
+            },
+            {
+                "text": "Wiesz ze",
+                "timestamp": 34692
+            },
+            {
+                "text": "Byles mi",
+                "timestamp": 35855
+            },
+            {
+                "text": "tak bliski",
+                "timestamp": 36553
+            },
+            {
+                "text": "Zegnaj",
+                "timestamp": 37967
+            },
+            {
+                "text": "Widzisz mnie",
+                "timestamp": 38879
+            },
+            {
+                "text": "ostatni raz",
+                "timestamp": 39655
+            },
+            {
+                "text": "Znow rani",
+                "timestamp": 41151
+            },
+            {
+                "text": "mi serce",
+                "timestamp": 41651
+            },
+            {
+                "text": "Strzelajac we mnie",
+                "timestamp": 42140
+            },
+            {
+                "text": "uderza piescia",
+                "timestamp": 43019
+            },
+            {
+                "text": "o stol",
+                "timestamp": 43719
+            },
+            {
+                "text": "On lubi te gierki",
+                "timestamp": 44605
+            },
+            {
+                "text": "kiedy go wielbia",
+                "timestamp": 45500
+            },
+            {
+                "text": "Ladne panienki",
+                "timestamp": 46324
+            },
+            {
+                "text": "u stop",
+                "timestamp": 47010
+            },
+            {
+                "text": "I troche to smieszy",
+                "timestamp": 47942
+            },
+            {
+                "text": "przyjaciel wierzy",
+                "timestamp": 48759
+            },
+            {
+                "text": "ze blad nie byl jego",
+                "timestamp": 49538
+            },
+            {
+                "text": "a moj",
+                "timestamp": 50312
+            },
+            {
+                "text": "Wstaje powoli",
+                "timestamp": 50849
+            },
+            {
+                "text": "wiem co robic",
+                "timestamp": 51656
+            },
+            {
+                "text": "Wychodze z tego",
+                "timestamp": 52518
+            },
+            {
+                "text": "jak krol",
+                "timestamp": 53411
+            },
+            {
+                "text": "Hej Wy",
+                "timestamp": 54728
+            },
+            {
+                "text": "Gdzie byliscie",
+                "timestamp": 55856
+            },
+            {
+                "text": "wczesniej",
+                "timestamp": 56718
+            },
+            {
+                "text": "Kiedy czulam",
+                "timestamp": 58138
+            },
+            {
+                "text": "bol i strach",
+                "timestamp": 59557
+            },
+            {
+                "text": "Dawniej",
+                "timestamp": 61325
+            },
+            {
+                "text": "Przyjaciele",
+                "timestamp": 62446
+            },
+            {
+                "text": "wszystkim",
+                "timestamp": 63305
+            },
+            {
+                "text": "Karty na stol",
+                "timestamp": 64194
+            },
+            {
+                "text": "i gramy",
+                "timestamp": 65710
+            },
+            {
+                "text": "VaBank",
+                "timestamp": 66544
+            },
+            {
+                "text": "Hej Wy",
+                "timestamp": 68064
+            },
+            {
+                "text": "Gdzie byliscie",
+                "timestamp": 69141
+            },
+            {
+                "text": "wczesniej",
+                "timestamp": 70012
+            },
+            {
+                "text": "Kiedy czulam",
+                "timestamp": 71604
+            },
+            {
+                "text": "bol i strach",
+                "timestamp": 72943
+            },
+            {
+                "text": "Dawniej",
+                "timestamp": 74628
+            },
+            {
+                "text": "Przyjaciele",
+                "timestamp": 75792
+            },
+            {
+                "text": "wszystkim",
+                "timestamp": 76582
+            },
+            {
+                "text": "Karty na stol",
+                "timestamp": 77521
+            },
+            {
+                "text": "i gramy",
+                "timestamp": 79043
+            },
+            {
+                "text": "VaBank",
+                "timestamp": 79845
+            },
+            {
+                "text": "______________________________________________",
+                "timestamp": 80911
+            },
+            {
+                "text": "Wybacz",
+                "timestamp": 84680
+            },
+            {
+                "text": "Juz",
+                "timestamp": 85534
+            },
+            {
+                "text": "zawiodlam",
+                "timestamp": 86198
+            },
+            {
+                "text": "sie",
+                "timestamp": 86675
+            },
+            {
+                "text": "na",
+                "timestamp": 87062
+            },
+            {
+                "text": "wszystkich",
+                "timestamp": 87386
+            },
+            {
+                "text": "Sprobuj",
+                "timestamp": 87997
+            },
+            {
+                "text": "Sprobuj",
+                "timestamp": 88470
+            },
+            {
+                "text": "szanse",
+                "timestamp": 88916
+            },
+            {
+                "text": "jedna dac",
+                "timestamp": 89825
+            },
+            {
+                "text": "Dawniej",
+                "timestamp": 91306
+            },
+            {
+                "text": "Dalbys mi",
+                "timestamp": 92116
+            },
+            {
+                "text": "ostatnia",
+                "timestamp": 92932
+            },
+            {
+                "text": "krople",
+                "timestamp": 93369
+            },
+            {
+                "text": "Teraz",
+                "timestamp": 94627
+            },
+            {
+                "text": "Bez Was",
+                "timestamp": 95500
+            },
+            {
+                "text": "jest",
+                "timestamp": 95925
+            },
+            {
+                "text": "ostatni",
+                "timestamp": 96304
+            },
+            {
+                "text": "akt",
+                "timestamp": 96679
+            },
+            {
+                "text": "Wolaj",
+                "timestamp": 97941
+            },
+            {
+                "text": "Krzycze",
+                "timestamp": 99117
+            },
+            {
+                "text": "glosno",
+                "timestamp": 99571
+            },
+            {
+                "text": "przez sen",
+                "timestamp": 99985
+            },
+            {
+                "text": "Cisza",
+                "timestamp": 101517
+            },
+            {
+                "text": "Tylko",
+                "timestamp": 102194
+            },
+            {
+                "text": "cisze",
+                "timestamp": 102616
+            },
+            {
+                "text": "umiesz dac",
+                "timestamp": 103026
+            },
+            {
+                "text": "Wiesz ze",
+                "timestamp": 103497
+            },
+            {
+                "text": "Byles mi",
+                "timestamp": 104928
+            },
+            {
+                "text": "tak bliski",
+                "timestamp": 106544
+            },
+            {
+                "text": "Zegnaj",
+                "timestamp": 108033
+            },
+            {
+                "text": "Widzisz",
+                "timestamp": 108761
+            },
+            {
+                "text": "mnie",
+                "timestamp": 109170
+            },
+            {
+                "text": "ostatni raz",
+                "timestamp": 109590
+            },
+            {
+                "text": "Znow rani",
+                "timestamp": 111161
+            },
+            {
+                "text": "mi serce",
+                "timestamp": 111591
+            },
+            {
+                "text": "Strzelajac",
+                "timestamp": 112048
+            },
+            {
+                "text": "we mnie",
+                "timestamp": 112510
+            },
+            {
+                "text": "uderza piescia",
+                "timestamp": 112956
+            },
+            {
+                "text": "o stol",
+                "timestamp": 113414
+            },
+            {
+                "text": "On lubi",
+                "timestamp": 114499
+            },
+            {
+                "text": "te gierki",
+                "timestamp": 114924
+            },
+            {
+                "text": "kiedy go wielbia",
+                "timestamp": 115415
+            },
+            {
+                "text": "Ladne panienki",
+                "timestamp": 116049
+            },
+            {
+                "text": "u stop",
+                "timestamp": 117044
+            },
+            {
+                "text": "I troche",
+                "timestamp": 118060
+            },
+            {
+                "text": "to smieszy",
+                "timestamp": 118545
+            },
+            {
+                "text": "przyjaciel wierzy",
+                "timestamp": 118996
+            },
+            {
+                "text": "ze blad",
+                "timestamp": 119532
+            },
+            {
+                "text": "nie byl jego",
+                "timestamp": 119965
+            },
+            {
+                "text": "a moj",
+                "timestamp": 120407
+            },
+            {
+                "text": "Wstaje powoli",
+                "timestamp": 120879
+            },
+            {
+                "text": "wiem co robic",
+                "timestamp": 121363
+            },
+            {
+                "text": "Wychodze z",
+                "timestamp": 122537
+            },
+            {
+                "text": "tego jak krol",
+                "timestamp": 123318
+            },
+            {
+                "text": "Hej Wy",
+                "timestamp": 124691
+            },
+            {
+                "text": "Gdzie byliscie",
+                "timestamp": 125855
+            },
+            {
+                "text": "wczesniej",
+                "timestamp": 126641
+            },
+            {
+                "text": "Kiedy czulam",
+                "timestamp": 128197
+            },
+            {
+                "text": "bol i strach",
+                "timestamp": 129572
+            },
+            {
+                "text": "Dawniej",
+                "timestamp": 131567
+            },
+            {
+                "text": "Przyjaciele",
+                "timestamp": 132450
+            },
+            {
+                "text": "wszystkim",
+                "timestamp": 133242
+            },
+            {
+                "text": "Karty na stol",
+                "timestamp": 134115
+            },
+            {
+                "text": "i gramy",
+                "timestamp": 135699
+            },
+            {
+                "text": "VaBank",
+                "timestamp": 136463
+            },
+            {
+                "text": "Hej Wy",
+                "timestamp": 137972
+            },
+            {
+                "text": "Gdzie byliscie",
+                "timestamp": 139119
+            },
+            {
+                "text": "wczesniej",
+                "timestamp": 139938
+            },
+            {
+                "text": "Kiedy czulam",
+                "timestamp": 141304
+            },
+            {
+                "text": "bol i strach",
+                "timestamp": 142866
+            },
+            {
+                "text": "Dawniej",
+                "timestamp": 144686
+            },
+            {
+                "text": "Przyjaciele",
+                "timestamp": 145734
+            },
+            {
+                "text": "wszystkim",
+                "timestamp": 146551
+            },
+            {
+                "text": "Karty na stol",
+                "timestamp": 147438
+            },
+            {
+                "text": "i gramy",
+                "timestamp": 148975
+            },
+            {
+                "text": "VaBank",
+                "timestamp": 149808
+            }
+        ];
